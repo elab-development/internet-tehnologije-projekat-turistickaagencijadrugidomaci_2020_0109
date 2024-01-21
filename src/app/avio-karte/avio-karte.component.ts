8474e1ff9adb3a5281e326f49e4fc5eaf1d2ed27
zm20200109 <zm20200109@student.fon.bg.ac.rs>
2024-01-21 16:46:04 +0100

    napravljen je scss fajl za avio-karta komponentu, prihvaceni su podaci iz home komponente o aviokartama i ispisani na html stranici avio-komponente

--- src/app/avio-karte/avio-karte.component.ts
+++ src/app/avio-karte/avio-karte.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Aviokarte } from './avio-karte/aviokarte';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-avio-karte',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './avio-karte.component.scss'
 })
 export class AvioKarteComponent {
+ //uobicajeni nacin u Angularu da prosledimo podatke iz app (roditeljske komp)
+  //u child komponentu
+  //! - property ce biti inicijalizovani i nece biti null ili undefined
+//aviokarteData: Aviokarte[] =[]
+@Input() aviokarta!:Aviokarte
+ //prihvatanje podataka od roditelja:
+@Input() aviokarteData: Aviokarte[]=[]; // input dekorator dobija child komponenta
+//kako bi pokupila podatke iz roditeljske komponente
+constructor(private route:ActivatedRoute){}
+//ispis podataka
+pagedAviokarteList: Aviokarte[]=[]; // aviokarte za jedan page
+pageSize: number = 3; //zelimo da imamo 3 aviokarte po stranici
 
+currentPage: number = 1;
+
+ngOnInit(): void {
+  this.route.queryParams.subscribe((params)=>{
+    if(params['aviokarteData']){
+        this.aviokarteData = JSON.parse(params['aviokarteData']);
+        //this.pageChanged(1);
+    }
+    
+  });
+  
+  //cim je ovo ispisan, on je primio te podatke
+  console.log('Aviokarte data is initialized with data:', this.aviokarteData) //to sto je ovaj dobio od home komponente, to se i ispisuje
+   //this.aviokarteData = this.aviokarteData;
+
+}
 }
