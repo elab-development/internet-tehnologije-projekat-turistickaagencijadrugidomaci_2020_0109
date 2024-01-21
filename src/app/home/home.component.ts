46b45eb900ebaad431a77157d6dacacf51a4bd0b
zm20200109 <zm20200109@student.fon.bg.ac.rs>
2024-01-21 16:26:30 +0100

    napravljen je .scss fajl za home komponentu, kao i js funkcionalnosti za prikupljanje mejla korisnika na dugme novo, promena home slike na klik slike iz galerije.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  prikupiPodatke(){
+    const userInput = prompt('Unesite svoj mejl kako biste dobijali najnovije ponude:');
+    if (userInput!==null){
+         console.log('Rezultat: ', userInput);
+    }
+  }
+ 
+   slike: string[] = ['../../assets/images/planina1.jpg', '../../assets/images/planina2.jpg', '../../assets/images/skijas.jpg']
+   selektovanaSlika: string = '../../assets/images/planina1.jpg'
+ 
+   prikaziSliku(slika:string){
+    this.selektovanaSlika = slika;   
+   }
+ 
+
+
+   
 
 }
